94b4d9ab0ebc834a0d45c5a4ca628950ab9107bd
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-25 17:18:24 -0400

    added list items and deleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
+const boxInput = document.querySelector('#new-task-description');
+const myList = document.querySelector('#tasks');
+const listForm = document.querySelector('#create-task-form');
+
+function addLiElement(e) {
+  e.preventDefault()
+  let task = document.createElement("li");
+  task.innerHTML = boxInput.value
+  let deleteButton = document.createElement('button');
+  deleteButton.innerText = "x";
+  deleteButton.addEventListener("click", () => {
+    task.remove();
+  })
+  task.append(deleteButton);
+  myList.append(task);
+  e.target.reset();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  listForm.addEventListener("submit", addLiElement
+})
+
+// listButton.addEventListener("click", function() {
+//   let newLi = document.createElement("li");
+//   newLi.innerText = boxInput.value;
+//   debugger
+//   myList.append(newLi);
+// })
